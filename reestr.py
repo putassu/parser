778042a506db8341770e8fb5bd0addb60f019f49
putassu <izopropylbenzol@gmail.com>
2021-02-09 00:21:29 +0300

    интерактивная версия консольного приложения

--- reestr.py
+++ reestr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[11]:
+# In[1]:
 
 
 import pandas as pd
@@ -14,42 +14,15 @@ import re
 months = {'Января':'01', 'Февраля':'02','Марта':'03','Апреля':'04','Мая':'05','Июня':'06','Июля':'07',
           'Августа':'08','Сентября':'09','Октября':'10','Ноября':'11','Декабря':'11'}
 import time
-from sys import argv
 
 
-# In[10]:
-
-
-#для вызова скрипта с параметрами: 
-#num_ - количество наименований ПО, которое необходимо распарсить(удобно для проверки)
-#
-#seconds_ - количество секунд задержки
-name, num_, filename_, frame_, seconds_ = argv
-try:
-    num = int(num_)
-except:
-    num = ''
-try:
-    frame = int(frame_)
-except:
-    frame = 500
-try:
-    seconds = int(seconds_)
-except:
-    seconds = 0
-try:
-    filename = filename_
-except:
-    filename = 'Reestr'
-
-
-# In[18]:
+# In[ ]:
 
 
 
 
 
-# In[12]:
+# In[2]:
 
 
 try:
@@ -64,7 +37,7 @@ else:
 baseurl = 'https://reestr.digital.gov.ru'
 
 
-# In[13]:
+# In[3]:
 
 
 #поиск количества страниц c отображением по 100
@@ -77,6 +50,7 @@ def find_n():
 # In[8]:
 
 
+print('Создание ссылок для всех страниц поиска на сайте реестра (с отображением по 100)')
 with open('links.txt', mode='w', encoding='utf-8') as myfile:
     for i in range(1, find_n()):
     # Создание ссылок для всех страниц поиска на сайте реестра (с отображением по 100)
@@ -89,16 +63,72 @@ with open('links.txt', mode='w', encoding='utf-8') as myfile:
             print(baseurl + line, file=myfile)
 
 
-# In[19]:
+# In[4]:
 
 
 # константные значения
 links = "links.txt"
 # количество уникальных названий ПО
+f = open(links)
+content = f.readlines()
+num = len(content)
+print(f'Всего ссылок: {num} \n введите номер ссылки, с которой начать парсинг(включительно):')
+try:
+    num0 = int(input()) - 1
+except:
+    print('введите целое число!')
+print('введите номер ссылки, на которой закончить парсинг(включительно):')
+try:
+    num1 = int(input())
+except:
+    print('введите целое число!')
+num = num1 - num0
+print(f'Будет распарсено {num} ссылок. Введите название файла:')
+try:
+    filename = input()
+except:
+    print('введите корректное название')
+print('Будем делать паузы после n ссылок? y/n')
+if input()=='y':
+    print('после скольки ссылок делать паузы?')
+    frame = int(input())
+    print('сколько секунд будут длится паузы?')
+    seconds = int(input())
+else:
+    frame = 1000
+    seconds = 0
+print('ну и последний вопрос: какой формат выходного файла желаете? 1 - xlsx, 2 - csv, 3 - оба')
+try:
+    format_ = int(input())
+except:
+    print('введите целое число (1, 2, 3) без посторонних символов и пробелов!')
+    
+# создание словаря для передачи его в последствии датафрейму в пандас
+values = {'Название по': [], 'Ссылка на ПО':[], 'Вид организации':[],'Название организации': [], 'Все продукты организации':[], 
+          'ИНН': [],'Сведения об исключительном праве':[],'Альтернатив': [], 'Класс по': [],  'Сайт': [], 'Дата регистрации':[], 'Рег.номер ПО':[],
+         'Дата решения':[],'Решение':[],'Ссылка на приказ':[]}
 
-if num == '':
-    with open(links, 'r') as f:
-        num = sum(1 for _ in f)
+values['Название по'] = ['' for element in range(num)]
+values['Ссылка на ПО'] = ['' for element in range(num)]
+values['Название организации'] = ['' for element in range(num)]
+values['ИНН'] = ['' for element in range(num)]
+values['Сведения об исключительном праве'] = ['' for element in range(num)]
+values['Сайт'] = ['' for element in range(num)]
+values['Альтернатив'] = ['' for element in range(num)]
+values['Класс по'] = ['' for element in range(num)]
+values['Вид организации'] = ['' for element in range(num)]
+values['Все продукты организации'] = ['' for element in range(num)]
+values['Дата регистрации'] = ['' for element in range(num)]
+values['Рег.номер ПО'] = ['' for element in range(num)]
+values['Дата решения'] = ['' for element in range(num)]
+values['Решение'] = ['' for element in range(num)]
+values['Ссылка на приказ'] = ['' for element in range(num)]
+
+
+# In[5]:
+
+
+
 
 # создание словаря для передачи его в последствии датафрейму в пандас
 values = {'Название по': [], 'Ссылка на ПО':[], 'Вид организации':[],'Название организации': [], 'Все продукты организации':[], 
@@ -125,26 +155,27 @@ values['Ссылка на приказ'] = ['' for element in range(num)]
 # In[6]:
 
 
-f = open(links)
-i = 0
-for url in f:
-    print(url.strip())
-    print(str(i+1) + ' of ' + str(num))
+
+j = 0
+for i in range(num0, num1):
+    url = content[i].strip()
+    print(url)
+    print(str(i+1) + ' of ' + str(num1))
     req = requests.get(url, verify=False).text
     soup = BeautifulSoup(req, 'lxml')
     # 1 название ПО
     soft = soup.find('h1', id='pagetitle').text
     print(soft + '\n')
-    values['Название по'][i] = soft
+    values['Название по'][j] = soft
     #15 ссылка на ПО
-    values['Ссылка на ПО'][i] = url
+    values['Ссылка на ПО'][j] = url
     # 2 название организации
     try:
         orgname = soup.find_all('a', title=re.compile("Все продукты"))
     except AttributeError:
         orgname = ""
         pass
-    values['Название организации'][i] = '::'.join([i.text.strip() for i in orgname])
+    values['Название организации'][j] = '::'.join([i.text.strip() for i in orgname])
     # 3 ИНН ^(\d{10}|\d{12})$
     try:
         inn = soup.find_all('div', text=re.compile("(\d{10}\s|\d{12}\s)"))
@@ -152,7 +183,7 @@ for url in f:
         inn = ''
         pass
     #инн может быть несколько, например в случае 4 правообладателей-людей, поэтому инн в списке
-    values['ИНН'][i] = '::'.join([i.text.strip() for i in inn])
+    values['ИНН'][j] = '::'.join([i.text.strip() for i in inn])
     # 4 альтернативное название продукта
     try:
         altname = str(soup.find('span', text=re.compile("Альтернативные наименования:")).find_parent('div')).split('<br/>')
@@ -163,7 +194,7 @@ for url in f:
     except AttributeError:
         altname = ""
         pass
-    values['Альтернатив'][i] = altname.strip()
+    values['Альтернатив'][j] = altname.strip()
     # 5 Класс по
     try:
         softclas = soup.find('span', text=re.compile("Класс ПО:")).find_parent('div').find_all('font')
@@ -171,14 +202,14 @@ for url in f:
     except AttributeError:
         softclass = ""
         pass
-    values['Класс по'][i] = softclass
+    values['Класс по'][j] = softclass
     # 6 сайт производителя
     try:
         site = soup.find('span', text=re.compile("Сайт производителя:")).find_parent('div').find('a').get('href')
     except AttributeError:
         site = ""
         pass
-    values['Сайт'][i] = site
+    values['Сайт'][j] = site
     # 7 вид организации
     try:
         type_of_organization = soup.find_all("h5", attrs={ "style" : "font-size: 20px; margin-bottom: 12px;"})
@@ -186,14 +217,14 @@ for url in f:
     except AttributeError:
         type_of_organization = ""
         pass
-    values['Вид организации'][i] = type_of_organization
+    values['Вид организации'][j] = type_of_organization
     # 8 все продукты организации
     try:
-        all_products = soup.find('a', title='Все продукты организации').get('href')
+        all_products = ['https://reestr.digital.gov.ru'+x.get('href') for x in soup.find_all('a', title=re.compile('Все продукты'))]
     except AttributeError:
         all_products = ""
         pass
-    values['Все продукты организации'][i] = 'https://reestr.digital.gov.ru' + all_products
+    values['Все продукты организации'][j] = '::'.join(all_products)
     # 9 дата регистрации
     try:
         date_of_registration = soup.find('span', text=re.compile("Дата регистрации:")).find_parent('div').text
@@ -205,7 +236,7 @@ for url in f:
     except AttributeError:
         temp3 = ""
         pass
-    values['Дата регистрации'][i] = temp3
+    values['Дата регистрации'][j] = temp3
     # 10 регистрационный номер ПО
     try:
         reg_number = soup.find('span', text=re.compile("Рег. номер ПО:")).find_parent('div').text
@@ -213,7 +244,7 @@ for url in f:
     except AttributeError:
         r = ""
         pass
-    values['Рег.номер ПО'][i] = r
+    values['Рег.номер ПО'][j] = r
     # 11 дата решения
     try:
         date_of_decision = soup.find('span', text=re.compile("Дата решения")).find_parent('div').text
@@ -225,39 +256,36 @@ for url in f:
     except AttributeError:
         tempp3 = ""
         pass
-    values['Дата решения'][i] = tempp3
+    values['Дата решения'][j] = tempp3
     # 12 решение
     try:
         decision = soup.find('span', text=re.compile("Решение уполномоченного")).find_parent('div').text
     except AttributeError:
         decision = ""
         pass
-    values['Решение'][i] = re.sub('Решение уполномоченного органа:', '', decision).strip()
+    values['Решение'][j] = re.sub('Решение уполномоченного органа:', '', decision).strip()
     # 13 ссылка на приказ минкомсвязи
     try:
         link = soup.find('span', text=re.compile("Ссылка на приказ")).find_parent('div').find('a').get('href')
     except AttributeError:
         link = ""
         pass
-    values['Ссылка на приказ'][i] = link
+    values['Ссылка на приказ'][j] = link
     # 14 сведения об исключительном праве
     try:
         svedeniya = soup.find('p', attrs = {'style':"line-height: 28px;"}).text.strip().replace('\n', '')
     except AttributeError:
         svedeniya = ''
         pass
-    values['Сведения об исключительном праве'][i] = svedeniya
-        
-    i = i + 1
-    if i>num-1:
-        break
-    elif i%frame ==0:
+    values['Сведения об исключительном праве'][j] = svedeniya
+    j = j + 1
+    if j%frame ==0:
         print(f'SLEEP {seconds} SECONDS')
         time.sleep(seconds)
 print("Парсинг прошел успешно. Записываю в файл")        
 
 
-# In[9]:
+# In[7]:
 
 
 df = pd.DataFrame(values)
@@ -265,6 +293,18 @@ columns = ['Наименование ПО', 'Ссылка на запись','В
 'Название правообладателя','Ссылка на все записи правообладателя','ИНН','Сведения об исключительном праве','Альтернативные наименования','Класс ПО','Сайт производителя',
 'Дата регистрации','Рег.номер ПО','Дата решения уполномоченного органа','Решение уполномоченного органа','Ссылка на приказ Минкомсвязи']
 df.columns = columns
-df.to_excel(f'{filename}.xlsx', index = False)
+if format_ == 1:
+    df.to_excel(f'{filename}.xlsx', index = False)
+elif format_ == 2:
+    df.to_csv(f'{filename}.csv', index = False)
+else:
+    df.to_excel(f'{filename}.xlsx', index = False)
+    df.to_csv(f'{filename}.csv', index = False)
 print('Запись файла произошла успешно!')
+
+
+# In[ ]:
+
+
+
 
